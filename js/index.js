2027f22913be3e08f02119bc3715a3cd94e3f16e
Eling486 <evanchen486@163.com>
2021-02-03 00:06:26 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.gacha_times = 0
+window.pool_name = ''
+
+function loadData(){
+    var path = './asstes/json/'
+}
+
+function gachaOnce(){
+
+}
+
+function gachaTenTimes(){
+
+}
+
+function gacha(){
+
+}
