e650729f4e4069324cad61f2fcfc08f774338d1b
Eling486 <evanchen486@163.com>
2021-02-05 15:07:39 +0800

    (nearly) have finished the gacha page

--- js/index.js
+++ js/index.js
@@ -59,10 +59,20 @@ function loadGachaPageRes() {
   function addSprite(ui_data, index, UI_res) {
     let _position = ui_data[index] // 下行用'0'分割字符串是用来进行相同纹理的元素区分的
     let _add = `${index} = new Sprite(${UI_res}["${index.split('0')[0]}.png"]);
-        app.stage.addChild(${index});
-        ${index}.scale.set(${_position.scale_x}, ${_position.scale_y});
-        ${index}.position.set(grid_w * ${_position.x} - ${index}.width / 2, grid_h * ${_position.y} - ${index}.height / 2)`;
-    eval(_add)
+          app.stage.addChild(${index});
+          ${index}.scale.set(${_position.scale_x}, ${_position.scale_y});
+          ${index}.position.set(grid_w * ${_position.x} - ${index}.width / 2, grid_h * ${_position.y} - ${index}.height / 2)`;
+    window.eval(_add)
+  }
+
+  function addText(text_data, index) {
+    let _position = text_data[index].position
+    let _add = `let ${index}_style = new TextStyle(${JSON.stringify(text_data[index].style)});
+          ${index} = new Text(${JSON.stringify(text_data[index].content)}, ${index}_style);
+          app.stage.addChild(${index});
+          ${index}.scale.set(${_position.scale_x}, ${_position.scale_y});
+          ${index}.position.set(grid_w * ${_position.x} - ${index}.width / 2, grid_h * ${_position.y} - ${index}.height / 2)`;
+    window.eval(_add)
   }
 
   function setup() {
@@ -82,27 +92,59 @@ function loadGachaPageRes() {
       addSprite(basic_ui_data.position, index, 'UI_basic')
     }
 
+    let alphaFilter = new PIXI.filters.AlphaFilter(); // 为详情框设置不透明度
+    alphaFilter.alpha = 0.5
+    details_box.filters = [alphaFilter]
+
     if (pool_ui_data.add_from_basic) {
       for (index in pool_ui_data.add_from_basic) {
         addSprite(pool_ui_data.add_from_basic, index, 'UI_basic')
       }
     }
-    let style = new TextStyle({
-      fontFamily: "Arial",
-      fontSize: 36,
-      fill: "white",
-      stroke: '#ff3300',
-      strokeThickness: 4,
-      dropShadow: true,
-      dropShadowColor: "#000000",
-      dropShadowBlur: 4,
-      dropShadowAngle: Math.PI / 6,
-      dropShadowDistance: 6,
-    });
-    /*
-    let message = new Text("Hello Pixi!", style);
-    app.stage.addChild(message);
-    */
+
+    for (index in basic_ui_data.text) {
+      addText(basic_ui_data.text, index)
+    }
+
+    if (pool_ui_data.text) {
+      for (index in pool_ui_data.text) {
+        addText(pool_ui_data.text, index)
+      }
+    }
+
+    btn_once.interactive = true;
+    btn_once.buttonMode = true
+    btn_once.on('pointerdown', function(){
+      btn_once.tint = 0xBBBBBB
+    })
+    btn_once.on('pointerup', function(){
+      btn_once.tint = 0xFFFFFF
+      gachaConfirm('once')
+    })
+    btn_once.on('pointerout', function(){
+      btn_once.tint = 0xFFFFFF
+    })
+
+    btn_ten_times.interactive = true;
+    btn_ten_times.buttonMode = true
+    btn_ten_times.on('pointerdown', function(){
+      btn_ten_times.tint = 0xBBBBBB
+    })
+    btn_ten_times.on('pointerup', function(){
+      btn_ten_times.tint = 0xFFFFFF
+      gachaConfirm('tentimes')
+    })
+    btn_ten_times.on('pointerout', function(){
+      btn_ten_times.tint = 0xFFFFFF
+    })
   }
 }
 
+function gachaConfirm(type){
+  if(type == 'once'){
+    
+  }
+  if(type == 'tentimes'){
+    
+  }
+}
